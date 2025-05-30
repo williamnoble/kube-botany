024a1280f4f6bab271ac199446718f6a6748ae9d
William Noble <27914124+williamnoble@users.noreply.github.com>
2025-05-30 16:14:50 +0100

    improve templates

--- server/template.go
+++ server/template.go
@@ -5,28 +5,13 @@ import (
 	"html/template"
 )
 
-// ParseTemplates parses HTML templates and stores them in the httpServer's templates map
-// It parses the "index" template from layout.html and index.html
-// It parses the "plant" template from layout.html and plant.html
-func (s *Server) ParseTemplates() {
-	//s.templates["index"] = template.Must(template.ParseFiles(
-	//	filepath.Join(s.templatesDir, "/layout.html"),
-	//	filepath.Join(s.templatesDir, "/index.html")))
-	//
-	//s.templates["plant"] = template.Must(template.ParseFiles(
-	//	filepath.Join(s.templatesDir, "/layout.html"),
-	//	filepath.Join(s.templatesDir, "/plant.html")))
-
-	s.templates["index"] = template.Must(template.ParseFS(templates.HtmlTemplates,
-		"layout.html",
-		"index.html"))
-
-	// Parse plant template (layout.html + plant.html)
-	s.templates["plant"] = template.Must(template.ParseFS(templates.HtmlTemplates,
-		"layout.html",
-		"plant.html"))
+// parseTemplate creates a new template from the embedded filesystem
+func (s *Server) parseTemplate(name string, files ...string) {
+	s.templates[name] = template.Must(template.ParseFS(templates.HtmlTemplates, files...))
 }
 
-func getTemplate() (*template.Template, error) {
-	return template.ParseFS(templates.HtmlTemplates, "index.html")
+// ParseTemplates initialises all templates used by the server
+func (s *Server) ParseTemplates() {
+	s.parseTemplate("index", "layout.html", "index.html")
+	s.parseTemplate("plant", "layout.html", "plant.html")
 }
